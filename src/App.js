0d4dcd282f4d67110f278edabafc92eca5347e49
linzheyuan <linzheyuan@baidu.com>
2020-04-23 23:23:32 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as monaco from 'monaco-editor';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: '// type your code...',
+    }
+  }
+  MonacoEnvironment = {
+    getWorkerUrl: function (moduleId, label) {
+      if (label === 'json') {
+        return './json.worker.js';
+      }
+      if (label === 'css') {
+        return './css.worker.js';
+      }
+      if (label === 'html') {
+        return './html.worker.js';
+      }
+      if (label === 'typescript' || label === 'javascript') {
+        return './ts.worker.js';
+      }
+      return './editor.worker.js';
+    },
+  };
+  componentDidMount(){
+    monaco.editor.create(document.getElementById('container'), {
+      value: [
+        'function x() {',
+        '\tconsole.log("Hello world!");',
+        '}'
+      ].join('\n'),
+      language: 'javascript'
+    });
+  }
+  editorDidMount(editor, monaco) {
+    console.log('editorDidMount', editor);
+    editor.focus();
+  }
+  onChange(newValue, e) {
+    console.log('onChange', newValue, e);
+  }
+  render() {
+    return (
+      <div id='container'>
+
+      </div>
+    );
+  }
 }
 
 export default App;
